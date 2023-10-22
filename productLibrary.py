0d0431a902c57faaa86b7725caa334a072753247
felipefrdly <104601475+felipefrdly@users.noreply.github.com>
2023-10-21 23:51:06 -0400

    Finished implementing stock statement
    Fully completed the stock statement method

--- productLibrary.py
+++ productLibrary.py
@@ -9,23 +9,35 @@ class Product:
         self._proCode = proCode
         self._proName = proName
         self._proSalePrice = proSalePrice
-        self.proManuCost = proManuCost
-        self.proStockLevel =  proStockLevel
-        self.approxMonthlyUnits = approxMonthlyUnits
+        self._proManuCost = proManuCost
+        self._proStockLevel =  proStockLevel
+        self._approxMonthlyUnits = approxMonthlyUnits
         
     def SimulateMonthlyProduction(self):
         pass
 
-    #this method simulates how many of the product are sold each month 
-    def SimulateProductionSales(self):
-        unitsSold = random.randint(self.approxMonthlyUnits -10 , self.approxMonthlyUnits + 10) #estimates the units sold by generating a number between 1 and the units manufactured +/- 10
-        return unitsSold
+    #THIS WAS A PAIN TO SET UP :(
+    def PredictStockStatement(self):
+        stock = self._approxMonthlyUnits + self._proStockLevel #sets the stock by adding the monthly units made and the current stock level
+        salesOfYear = 0 #empty variable for storing yearly sales
+        manufactAYear = 0 #empty variable for storing manufacturing total
 
-    def PredictStockStatement(self, totalunitsSold):
-        monthlyStock = self.approxMonthlyUnits - totalunitsSold
-        return monthlyStock
+        for i in range(1,13):
+            unitsSold = random.randint(self._approxMonthlyUnits -10 , self._approxMonthlyUnits + 10) #estimates the units sold by generating a number between 1 and the units manufactured +/- 10
+            newStock = stock - unitsSold #creates a variable to store the stock after the units are sold
+            print(f"Month {i}:") #Display the month
+            print(f"Manufactured: {self._approxMonthlyUnits}") #Display the manufactured units
+            print(f"Sold: {unitsSold}") #displays the units sold
+            print(f"Stock: {newStock}\n") #displays the new stock
+            stock = newStock + self._approxMonthlyUnits #updates the stock for next month by adding the monthly units
+            
+            salesOfYear += unitsSold #adds the units sold that month to the total
+            manufactAYear += self._approxMonthlyUnits #adds the units manufactured to the total
+            
+        netProfit = (salesOfYear * self._proSalePrice) - (manufactAYear * self._proManuCost) #forumla that calculates net profit
+        print(f"Net Proftis: ${round(netProfit, 2)} CAD") #displays net profit
 
-test = Product(1,1,1,1,1,100)
+test = Product(100,"soccerBall", 250.99, 200.99, 100, 100)
 
-print(test.SimulateProductionSales())
-test.PredictStockStatement(test.SimulateProductionSales())
+#print(test.SimulateProductionSales())
+test.PredictStockStatement()
