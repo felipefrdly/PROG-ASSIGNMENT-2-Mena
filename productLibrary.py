86fa94b731be7da5f4fc299b8fc9c6ecb718ae37
felipefrdly <104601475+felipefrdly@users.noreply.github.com>
2023-10-21 21:05:45 -0400

    created the calculations for 2 of the methods in the program
    did the math for production sales and began working on the stock statement

--- productLibrary.py
+++ productLibrary.py
@@ -1,6 +1,7 @@
 #Felipe Mena
 #10/21/2023
 #This program is designed to calculate and simulate sales metrics of diffrent products that a user creates. this file contains all objects that the program revolves around
+import random
 
 class Product:
     #initalizes all variables on objects creation
@@ -12,11 +13,19 @@ class Product:
         self.proStockLevel =  proStockLevel
         self.approxMonthlyUnits = approxMonthlyUnits
         
-    def SimulateMonthlyProduction():
+    def SimulateMonthlyProduction(self):
         pass
 
-    def SimulateProductionSales():
-        pass
+    #this method simulates how many of the product are sold each month 
+    def SimulateProductionSales(self):
+        unitsSold = random.randint(self.approxMonthlyUnits -10 , self.approxMonthlyUnits + 10) #estimates the units sold by generating a number between 1 and the units manufactured +/- 10
+        return unitsSold
 
-    def PredictStockStatement():
-        pass
+    def PredictStockStatement(self, totalunitsSold):
+        monthlyStock = self.approxMonthlyUnits - totalunitsSold
+        return monthlyStock
+
+test = Product(1,1,1,1,1,100)
+
+print(test.SimulateProductionSales())
+test.PredictStockStatement(test.SimulateProductionSales())
